ea006f92bbcc72b5e7b9a594303a8b88f866924f
JP <85654561+jp524@users.noreply.github.com>
2023-07-17 15:53:13 -0400

    Build job to pull repositories from GitHub

---
+++ app/jobs/pull_github_repo_job.rb
@@ -0,0 +1,29 @@
+class PullGithubRepoJob < ApplicationJob
+  def perform(repository)
+    directory = Rails.root.join('repos', repository.owner, repository.name)
+    if Dir.exist?(directory)
+      Git.open(directory).pull
+    else
+      Git.clone(repository.git_url, directory, options: { branch: repository.branch })
+    end
+    description = get_description(repository.owner, repository.name, repository.token)
+    update_repository(repository.id, description)
+  end
+
+  private
+
+  def update_repository(repository_id, description)
+    repository = Repository.find(repository_id)
+    repository.update(last_pull_at: DateTime.current, description:)
+  end
+
+  def get_description(owner, name, token)
+    client = if token.blank?
+               Octokit::Client.new
+             else
+               Octokit::Client.new(access_token: token)
+             end
+    repo = client.repository("#{owner}/#{name}")
+    repo.description
+  end
+end
---
+++ spec/jobs/pull_github_repo_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe PullGithubRepoJob, type: :job do
+  include ActiveJob::TestHelper
+
+  before(:all) do
+    @repo = Repository.create(owner: 'jp524', name: 'markdown-templates')
+  end
+
+  subject(:job) { described_class.perform_later(@repo) }
+
+  it 'queues the job' do
+    expect { job }.to change(ActiveJob::Base.queue_adapter.enqueued_jobs, :size).by(1)
+  end
+
+  it 'executes perform' do
+    expect(@repo.description).to be_nil
+    expect(@repo.last_pull_at).to be_nil
+
+    perform_enqueued_jobs { job }
+    @repo.reload
+
+    expect(@repo.description).to eq('Files templates for testing')
+    expect(@repo.last_pull_at).not_to be_nil
+  end
+
+  after do
+    clear_enqueued_jobs
+    clear_performed_jobs
+  end
+end
