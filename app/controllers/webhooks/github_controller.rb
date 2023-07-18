e0e0f577589fa35b4ce73560da8a09b869352d88
JP <85654561+jp524@users.noreply.github.com>
2023-07-18 13:51:24 -0400

    Build Webhooks::Github controller

---
+++ app/controllers/webhooks/github_controller.rb
@@ -0,0 +1,27 @@
+class Webhooks::GithubController < ApplicationController
+  skip_forgery_protection
+
+  before_action :verify_event
+
+  def create
+    head :ok
+
+    case request.headers['X-GitHub-Event']
+    when 'ping'
+      flash.now[:notice] = 'Webhook successfully created.'
+    when 'push'
+      repository = Repository.find_by(name: params[:repository][:name], owner: params[:repository][:owner][:name])
+      PullGithubRepoJob.perform_later(repository)
+    end
+  end
+
+  private
+
+  def verify_event
+    secret = Rails.application.credentials.webhook_secret
+    signature = "sha256=#{OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha256'), secret, request.raw_post)}"
+    unless ActiveSupport::SecurityUtils.secure_compare(signature, request.headers['X-Hub-Signature-256'])
+      head :bad_request
+    end
+  end
+end
---
+++ spec/requests/webhooks_github/create_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+describe 'POST /webhooks/github' do
+  scenario 'X-GitHub-Event: ping' do
+    secret = Rails.application.credentials.webhook_secret
+    data = 'zen=Responsive+is+better+than+fast.'
+    signature = "sha256=#{OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha256'), secret, data)}"
+
+    post '/webhooks/github',
+         params: { 'zen': 'Responsive is better than fast.' },
+         headers: {
+           'X-GitHub-Event': 'ping',
+           'X-Hub-Signature-256': signature
+         }
+
+    expect(response.status).to eq(200)
+  end
+
+  scenario 'X-GitHub-Event: push' do
+    secret = Rails.application.credentials.webhook_secret
+    data = 'repository[name]=repo_name&repository[owner][name]=owner_name'
+    signature = "sha256=#{OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha256'), secret, data)}"
+
+    post '/webhooks/github',
+         params: {
+           'repository': {
+             'name': 'repo_name',
+             'owner': {
+               'name': 'owner_name'
+             }
+           }
+         },
+         headers: {
+           'X-GitHub-Event': 'push',
+           'X-Hub-Signature-256': signature
+         }
+
+    expect(response.status).to eq(200)
+  end
+end
