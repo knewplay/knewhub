806709ff05e36de6e0f5f8e7e10123b129b2a6ce
JP <85654561+jp524@users.noreply.github.com>
2023-07-24 11:10:31 -0400

    Add HTTP codes to responses from CollectionsController

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -3,18 +3,23 @@ class CollectionsController < ApplicationController
   before_action :modify_view_path
 
   def show
-    @file_path = "#{params[:owner]}/#{params[:name]}/#{params[:path]}"
+    file_path = "#{params[:owner]}/#{params[:name]}/#{params[:path]}"
+    return head :not_found unless file_exists?(file_path)
+
     respond_to do |format|
-      format.html { render @file_path }
+      format.html { render file_path }
       format.any(:png, :jpg, :jpeg) do
-        send_file "#{Rails.root}/repos/#{@file_path}.#{request.format.to_sym}"
+        send_file "#{Rails.root}/repos/#{file_path}.#{request.format.to_sym}"
       end
-      format.all { render html: 'This file format cannot be rendered' }
+      format.all { head :not_implemented }
     end
   end
 
   def index
-    render "#{params[:owner]}/#{params[:name]}/index"
+    file_path = "#{params[:owner]}/#{params[:name]}/index"
+    return head :not_found unless file_exists?(file_path)
+
+    render file_path
   end
 
   private
@@ -25,5 +30,10 @@ class CollectionsController < ApplicationController
 
   def modify_view_path
     prepend_view_path "#{Rails.root}/repos"
+  end
+
+  def file_exists?(file_path)
+    File.exist?("#{Rails.root}/repos/#{file_path}.md") \
+    || File.exist?("#{Rails.root}/repos/#{file_path}.#{request.format.to_sym}")
   end
 end
