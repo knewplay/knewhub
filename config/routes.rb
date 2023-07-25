3dd94c5d0b7652036187de69e84d204857fd5e5f
Jade P <85654561+jp524@users.noreply.github.com>
2023-07-25 13:04:03 -0400

    Render Markdown files (#4)
    * Render Markdown files, including embedded images and code files
    
    * Write tests for rendering Markdown files
    
    * Render index.md page at book's root URL
    
    * Refactor RepositoriesController to use application layout
    
    * Refactor RepositoriesController#show based on new format of index.md
    
    * Move RepositoriesController #show and #main to CollectionsController
    
    * Add HTTP codes to responses from CollectionsController
    
    * Create class RequestPath to store and define request url
    
    * Add support for gif, svg and webp image formats in CollectionsController

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   resources :repositories, only: %i[index new create]
   root 'repositories#index'
 
+  get 'collections/:owner/:name/pages/index', to: 'collections#index'
+  get 'collections/:owner/:name/pages/*path', to: 'collections#show'
+
   namespace :webhooks do
     resource :github, controller: :github, only: [:create]
   end
---
+++ spec/systems/collections/show_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'show collection', type: :system do
+  scenario 'displays Markdown text in HTML' do
+    visit '/collections/jp524/markdown-templates/pages/chapter-1/chapter-1-article-1'
+
+    assert_selector 'h1', text: 'Non anser honore ornique'
+  end
+
+  scenario 'displays embedded images' do
+    visit '/collections/jp524/markdown-templates/pages/chapter-1/chapter-1-article-1'
+    expect(page).to have_css("img[alt='Ruby on Rails logo']")
+  end
+
+  scenario 'displays embedded code files' do
+    visit '/collections/jp524/markdown-templates/pages/chapter-2/chapter-2-article-1'
+
+    assert_selector 'p', text: 'File: ./code-files/code-example.c'
+    assert_selector 'code', text: "void main() {\n  hello world\n}"
+  end
+end
