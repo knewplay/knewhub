144017cdb7c57b81db23881684ab2588dc16627e
JP <85654561+jp524@users.noreply.github.com>
2023-07-26 14:40:28 -0400

    Add Author model and configure OmniAuth gem

--- Gemfile
+++ Gemfile
@@ -60,6 +60,10 @@ gem 'git'
 # Toolkit for the GitHub API
 gem 'octokit'
 
+# Authentication using OmniAuth
+gem 'omniauth-github'
+gem 'omniauth-rails_csrf_protection'
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_24_194432) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_26_150309) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
+
+  create_table "authors", force: :cascade do |t|
+    t.string "github_uid"
+    t.string "github_username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "repositories", force: :cascade do |t|
     t.string "owner"
