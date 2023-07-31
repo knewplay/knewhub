858038e9ee8757de32786811bb04f3fedbb51738
JP <85654561+jp524@users.noreply.github.com>
2023-07-31 10:01:48 -0400

    Refactor test for CreateGithubWebhookJob using VCR

--- spec/jobs/create_github_webhook_spec.rb
+++ spec/jobs/create_github_webhook_spec.rb
@@ -17,10 +17,12 @@ RSpec.describe CreateGithubWebhookJob, type: :job do
   end
 
   it 'executes perform' do
-    Sidekiq::Testing.inline! do
-      expect(@repo.description).to be_nil
-      expect(@repo.last_pull_at).to be_nil
-      CreateGithubWebhookJob.perform_async(@repo.uuid, @repo.name, @repo.author.github_username, @repo.token)
+    VCR.use_cassette('create_github_webhook') do
+      Sidekiq::Testing.inline! do
+        expect(@repo.description).to be_nil
+        expect(@repo.last_pull_at).to be_nil
+        CreateGithubWebhookJob.perform_async(@repo.uuid, @repo.name, @repo.author.github_username, @repo.token)
+      end
     end
   end
 end
