49c7dc28c1aa695d22afb54b2bffdd3d9595ba41
JP <85654561+jp524@users.noreply.github.com>
2023-08-02 09:45:01 -0400

    Clean up files generated by Administrate gem

--- app/dashboards/author_dashboard.rb
+++ app/dashboards/author_dashboard.rb
@@ -1,26 +1,16 @@
-require "administrate/base_dashboard"
+require 'administrate/base_dashboard'
 
 class AuthorDashboard < Administrate::BaseDashboard
-  # ATTRIBUTE_TYPES
-  # a hash that describes the type of each of the model's fields.
-  #
-  # Each different type represents an Administrate::Field object,
-  # which determines how the attribute is displayed
-  # on pages throughout the dashboard.
   ATTRIBUTE_TYPES = {
     id: Field::Number,
     github_uid: Field::String,
     github_username: Field::String,
     repositories: Field::HasMany,
     created_at: Field::DateTime,
-    updated_at: Field::DateTime,
+    updated_at: Field::DateTime
   }.freeze
 
-  # COLLECTION_ATTRIBUTES
-  # an array of attributes that will be displayed on the model's index page.
-  #
-  # By default, it's limited to four items to reduce clutter on index pages.
-  # Feel free to add, remove, or rearrange items.
+  # Attributes that will be displayed on the model's index page.
   COLLECTION_ATTRIBUTES = %i[
     id
     github_uid
@@ -28,8 +18,7 @@ class AuthorDashboard < Administrate::BaseDashboard
     repositories
   ].freeze
 
-  # SHOW_PAGE_ATTRIBUTES
-  # an array of attributes that will be displayed on the model's show page.
+  # Attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = %i[
     id
     github_uid
@@ -39,25 +28,14 @@ class AuthorDashboard < Administrate::BaseDashboard
     updated_at
   ].freeze
 
-  # FORM_ATTRIBUTES
-  # an array of attributes that will be displayed
-  # on the model's form (`new` and `edit`) pages.
+  # Attributes that will be displayed on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = %i[
     github_uid
     github_username
     repositories
   ].freeze
 
-  # COLLECTION_FILTERS
-  # a hash that defines filters that can be used while searching via the search
-  # field of the dashboard.
-  #
-  # For example to add an option to search for open resources by typing "open:"
-  # in the search field:
-  #
-  #   COLLECTION_FILTERS = {
-  #     open: ->(resources) { resources.where(open: true) }
-  #   }.freeze
+  # Filters that can be used while searching via the search field of the dashboard.
   COLLECTION_FILTERS = {}.freeze
 
   # Overwrite this method to customize how authors are displayed
