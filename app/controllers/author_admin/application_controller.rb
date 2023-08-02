5eae94088d649601973a6a6eefbde0e83792458b
JP <85654561+jp524@users.noreply.github.com>
2023-08-02 09:37:38 -0400

    Refactor code associated with Author authentication into a concern

--- app/controllers/author_admin/application_controller.rb
+++ app/controllers/author_admin/application_controller.rb
@@ -1,19 +1,6 @@
 module AuthorAdmin
   class ApplicationController < Administrate::ApplicationController
-    protect_from_forgery with: :exception
-    helper_method :current_author
+    include AuthorAuthentication
     before_action :require_author_authentication
-
-    def require_author_authentication
-      redirect_to root_path, alert: 'Requires authentication' unless author_signed_in?
-    end
-
-    def current_author
-      @current_author ||= Author.find(session[:author_id]) if session[:author_id]
-    end
-
-    def author_signed_in?
-      !!current_author
-    end
   end
 end
