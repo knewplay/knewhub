e3d6d46250fac4d0847beac8553179221fde1dd1
JP <85654561+jp524@users.noreply.github.com>
2023-08-02 09:45:01 -0400

    Clean up files generated by Administrate gem

--- app/dashboards/repository_dashboard.rb
+++ app/dashboards/repository_dashboard.rb
@@ -1,12 +1,6 @@
-require "administrate/base_dashboard"
+require 'administrate/base_dashboard'
 
 class RepositoryDashboard < Administrate::BaseDashboard
-  # ATTRIBUTE_TYPES
-  # a hash that describes the type of each of the model's fields.
-  #
-  # Each different type represents an Administrate::Field object,
-  # which determines how the attribute is displayed
-  # on pages throughout the dashboard.
   ATTRIBUTE_TYPES = {
     id: Field::Number,
     branch: Field::String,
@@ -18,14 +12,10 @@ class RepositoryDashboard < Administrate::BaseDashboard
     token: Field::Password,
     uuid: Field::String,
     created_at: Field::DateTime,
-    updated_at: Field::DateTime,
+    updated_at: Field::DateTime
   }.freeze
 
-  # COLLECTION_ATTRIBUTES
-  # an array of attributes that will be displayed on the model's index page.
-  #
-  # By default, it's limited to four items to reduce clutter on index pages.
-  # Feel free to add, remove, or rearrange items.
+  # Attributes that will be displayed on the model's index page.
   COLLECTION_ATTRIBUTES = %i[
     id
     name
@@ -34,8 +24,7 @@ class RepositoryDashboard < Administrate::BaseDashboard
     last_pull_at
   ].freeze
 
-  # SHOW_PAGE_ATTRIBUTES
-  # an array of attributes that will be displayed on the model's show page.
+  # Attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = %i[
     name
     title
@@ -45,9 +34,7 @@ class RepositoryDashboard < Administrate::BaseDashboard
     updated_at
   ].freeze
 
-  # FORM_ATTRIBUTES
-  # an array of attributes that will be displayed
-  # on the model's form (`new` and `edit`) pages.
+  # Attributes that will be displayed on the model's form new page.
   FORM_ATTRIBUTES_NEW = %i[
     name
     title
@@ -55,22 +42,14 @@ class RepositoryDashboard < Administrate::BaseDashboard
     token
   ].freeze
 
+  # Attributes that will be displayed on the model's form edit page.
   FORM_ATTRIBUTES_EDIT = %i[
     name
     title
     branch
   ].freeze
 
-  # COLLECTION_FILTERS
-  # a hash that defines filters that can be used while searching via the search
-  # field of the dashboard.
-  #
-  # For example to add an option to search for open resources by typing "open:"
-  # in the search field:
-  #
-  #   COLLECTION_FILTERS = {
-  #     open: ->(resources) { resources.where(open: true) }
-  #   }.freeze
+  # Filters that can be used while searching via the search field of the dashboard.
   COLLECTION_FILTERS = {}.freeze
 
   # Overwrite this method to customize how repositories are displayed
