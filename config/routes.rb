1ba0f2cedcee30b147a1c61d347644726b1bcb76
JP <85654561+jp524@users.noreply.github.com>
2023-08-03 10:07:13 -0400

    Refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   root 'static_pages#index'
 
   scope module: 'author_admin', path: 'author', as: 'author_admin' do
-    resources :repositories
+    resources :repositories do
+      post 'rebuild', on: :member
+    end
     root 'repositories#index'
   end
-
-  post 'author_admin/repositories/:id/rebuild', to: 'author_admin/repositories#rebuild', as: 'rebuild_repository'
 
   post '/webhooks/github/:uuid', to: 'webhooks/github#create'
 
