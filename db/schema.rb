cc75ddbcf1b156af2ed1f71bb33c2323f925f25f
JP <85654561+jp524@users.noreply.github.com>
2023-08-16 09:36:58 -0400

    Add 'name' column to Authors table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_01_174922) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_16_124805) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_01_174922) do
     t.string "github_username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
   create_table "repositories", force: :cascade do |t|
