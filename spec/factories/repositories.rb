dcfd101893d4cb60c6fa1b4dec112105d1d2e529
JP <85654561+jp524@users.noreply.github.com>
2023-08-16 15:02:03 -0400

    Refactor job tests to use factories

--- spec/factories/repositories.rb
+++ spec/factories/repositories.rb
@@ -4,5 +4,12 @@ FactoryBot.define do
     token { 'ghp_abcde12345' }
     title { 'Test Repo' }
     association :author
+
+    trait :real do
+      name { 'test-repo' }
+      token { Rails.application.credentials.pat }
+      title { 'Test Repo' }
+      association :author, :real
+    end
   end
 end
