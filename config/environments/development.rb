9d1e5c9b4d36cbe2646c0a6300ec236d599ddda3
JP <85654561+jp524@users.noreply.github.com>
2023-08-21 14:41:09 -0400

    Update Rails version to 7.0.7

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,5 +67,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # Add ngrok host used to receive webhooks
   config.hosts << Rails.application.credentials.host_url
 end
