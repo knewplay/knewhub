6a95fe815b1d353ee4726139fd0057c4bddf70ec
JP <85654561+jp524@users.noreply.github.com>
2023-08-22 13:39:28 -0400

    Truncate title on SystemDashboards & AuthorDashboards::Repositories#index

--- app/controllers/system_dashboards/repositories_controller.rb
+++ app/controllers/system_dashboards/repositories_controller.rb
@@ -4,6 +4,10 @@ module SystemDashboards
     # to be able to call background jobs
     include DashboardRepositories
 
+    def after_resource_updated_path(_requested_resource)
+      system_dashboards_repositories_path
+    end
+
     # See https://administrate-demo.herokuapp.com/customizing_controller_actions
     # for more information
   end
