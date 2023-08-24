a67e4432d053e4cfd212752728a685734873f25b
JP <85654561+jp524@users.noreply.github.com>
2023-08-24 14:38:49 -0400

    Sort SystemDashboards::Repositories by ID

--- app/controllers/system_dashboards/repositories_controller.rb
+++ app/controllers/system_dashboards/repositories_controller.rb
@@ -2,5 +2,9 @@ module SystemDashboards
   class RepositoriesController < SystemDashboards::ApplicationController
     # See https://administrate-demo.herokuapp.com/customizing_controller_actions
     # for more information
+
+    def default_sorting_attribute
+      :id
+    end
   end
 end
