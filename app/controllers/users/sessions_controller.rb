24fb77f35a942de07666c9313723dd30dba81fbe
JP <85654561+jp524@users.noreply.github.com>
2023-08-31 11:14:30 -0400

    Clean up code

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,11 +1,9 @@
 class Users::SessionsController < Devise::SessionsController
-  # POST /resource/sign_in
   def create
     super
     session[:administrator_id] = nil if session[:administrator_id]
   end
 
-  # DELETE /resource/sign_out
   def destroy
     super
     session[:author_id] = nil
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
 
+  # Override method to set remember_me == true by default
   def remember_me
     super.nil? ? true : super
   end
 
   protected
 
+  # Override method to allow users to set up password after email confirmation
   def password_required?
     confirmed? ? super : false
   end
