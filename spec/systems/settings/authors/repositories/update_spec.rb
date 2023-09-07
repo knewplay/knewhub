1c253c254b9754a599b67193c3d48c2ea791e759
JP <85654561+jp524@users.noreply.github.com>
2023-09-07 10:39:59 -0400

    Write tests for Build with action:update

--- spec/systems/settings/authors/repositories/update_spec.rb
+++ spec/systems/settings/authors/repositories/update_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'AuthorSpace::Repositories #update', type: :system do
+RSpec.describe 'AuthorSpace::Repositories#update', type: :system do
   let(:repo) { create(:repository) }
   let(:author) { repo.author }
 
@@ -36,6 +36,61 @@ RSpec.describe 'AuthorSpace::Repositories #update', type: :system do
 
       repo.reload
       expect(repo.branch).to eq('other_branch')
+    end
+
+    context 'updating the name and title using the Build process' do
+      before(:all) do
+        # Creates and clones a repository
+        @repo = create(:repository, :real)
+        clone_build = create(:build, repository: @repo)
+        Sidekiq::Testing.inline! do
+          VCR.use_cassette('clone_github_repo') do
+            CloneGithubRepoJob.perform_async(@repo.id, clone_build.id)
+          end
+        end
+
+        # Updates repository with a new mame and title
+        author = @repo.author
+        sign_in author.user
+        page.set_rack_session(author_id: author.id)
+
+        visit edit_settings_author_repository_path(@repo.id)
+        fill_in('Name', with: 'markdown-templates')
+        fill_in('Title', with: 'Markdown Templates')
+        click_on 'Update Repository'
+
+        sleep(1)
+        @update_build = @repo.builds.last
+
+        Sidekiq::Testing.inline! do
+          VCR.use_cassette('clone_github_repo_for_update') do
+            CloneGithubRepoJob.perform_async(@repo.id, @update_build.id)
+          end
+        end
+      end
+
+      scenario "creates an associated Build with action 'update'" do
+        expect(@update_build.action).to eq('update')
+      end
+
+      scenario 'creates the first log' do
+        expect(@update_build.logs.first.content).to eq('Repository successfully cloned.')
+      end
+
+      scenario 'creates the second log' do
+        expect(@update_build.logs.second.content).to eq('Repository description successfully updated from GitHub.')
+      end
+
+      scenario "sets Build status to 'Complete'" do
+        @update_build.reload
+        expect(@update_build.status).to eq('Complete')
+      end
+
+      after(:all) do
+        @repo.reload
+        directory = Rails.root.join('repos', @repo.author.github_username, @repo.name)
+        FileUtils.remove_dir(directory)
+      end
     end
   end
 
