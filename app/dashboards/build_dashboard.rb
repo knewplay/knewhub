cc06c1358763024fbb05c927ae766e65757d4357
Jade <85654561+jp524@users.noreply.github.com>
2023-09-11 13:40:11 -0400

    Display Builds and Logs on Dashboard

---
+++ app/dashboards/build_dashboard.rb
@@ -0,0 +1,51 @@
+require 'administrate/base_dashboard'
+
+class BuildDashboard < Administrate::BaseDashboard
+  ATTRIBUTE_TYPES = {
+    id: Field::Number,
+    action: Field::String,
+    completed_at: Field::DateTime,
+    logs: Field::HasMany,
+    repository: Field::BelongsTo.with_options(
+      searchable: true,
+      searchable_fields: %w[id name]
+    ),
+    status: Field::String,
+    created_at: Field::DateTime,
+    updated_at: Field::DateTime
+  }.freeze
+
+  # Attributes that will be displayed on the model's index page.
+  COLLECTION_ATTRIBUTES = %i[
+    id
+    repository
+    action
+    created_at
+    completed_at
+    status
+  ].freeze
+
+  # Attributes that will be displayed on the model's show page.
+  SHOW_PAGE_ATTRIBUTES = %i[
+    id
+    repository
+    action
+    created_at
+    completed_at
+    status
+    logs
+  ].freeze
+
+  # Attributes that will be displayed on the model's form (`new` and `edit`) pages.
+  FORM_ATTRIBUTES = %i[].freeze
+
+  # Filters that can be used while searching via the search field of the dashboard.
+  COLLECTION_FILTERS = {}.freeze
+
+  # Overwrite this method to customize how builds are displayed
+  # across all pages of the admin dashboard.
+  #
+  # def display_resource(build)
+  #   "Build ##{build.id}"
+  # end
+end
---
+++ app/dashboards/log_dashboard.rb
@@ -0,0 +1,37 @@
+require 'administrate/base_dashboard'
+
+class LogDashboard < Administrate::BaseDashboard
+  ATTRIBUTE_TYPES = {
+    id: Field::Number,
+    build: Field::BelongsTo,
+    content: Field::Text.with_options(
+      truncate: 150
+    ),
+    failure: Field::Boolean,
+    created_at: Field::DateTime,
+    updated_at: Field::DateTime
+  }.freeze
+
+  # Attributes that will be displayed on the model's index page.
+  COLLECTION_ATTRIBUTES = %i[
+    id
+    content
+    failure
+  ].freeze
+
+  # Attributes that will be displayed on the model's show page.
+  SHOW_PAGE_ATTRIBUTES = %i[].freeze
+
+  # Attributes that will be displayed on the model's form (`new` and `edit`) pages.
+  FORM_ATTRIBUTES = %i[].freeze
+
+  # Filters that can be used while searching via the search field of the dashboard.
+  COLLECTION_FILTERS = {}.freeze
+
+  # Overwrite this method to customize how logs are displayed
+  # across all pages of the admin dashboard.
+  #
+  # def display_resource(log)
+  #   "Log ##{log.id}"
+  # end
+end
