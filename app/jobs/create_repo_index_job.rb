776df2edea04491bc94c708730d0c89ed047a81a
Jade <85654561+jp524@users.noreply.github.com>
2023-09-11 09:28:04 -0400

    Generate front-matter in CreateRepoIndexJob

--- app/jobs/create_repo_index_job.rb
+++ app/jobs/create_repo_index_job.rb
@@ -2,14 +2,16 @@ class CreateRepoIndexJob
   include Sidekiq::Job
 
   def perform(repository_id)
-    _repository, directory = RepositoryDirectory.define(repository_id)
+    repository, directory = RepositoryDirectory.define(repository_id)
     return if index_file_exists?(directory)
 
     markdown_files_location = list_markdown_file_location(directory)
     links = generate_links(markdown_files_location)
+    front_matter = generate_front_matter(repository)
+    content = "#{front_matter}\n#{links}"
 
     filepath = File.join(directory, 'index.md')
-    File.open(filepath, 'w') { |f| f.write(links) }
+    File.open(filepath, 'w') { |f| f.write(content) }
   end
 
   def index_file_exists?(directory)
@@ -25,17 +27,30 @@ class CreateRepoIndexJob
   end
 
   # Returns string in the following format:
-  # "[Folder/Filename](./folder/filename)
-  #  [Folder/Second Folder/File Name](.folder/second%20folder/file_name)"
+  # "* [Folder/Filename](./folder/filename)
+  #  * [Folder/Second Folder/File Name](.folder/second%20folder/file_name)"
   def generate_links(markdown_files_location, content = '')
     markdown_files_location.map do |file_location|
       filename = file_location.titleize
-      content << "[#{filename}](./#{file_location.gsub(/\s/, '%20')})\n"
+      content << "* [#{filename}](./#{file_location.gsub(/\s/, '%20')})\n"
     end
     content
   end
 
-  # def generate_front_matter
-  #   TBD
-  # end
+  def generate_front_matter(repository)
+    <<~CONTENT
+      ---
+      title: #{repository.name.titleize}
+      date: #{repository.last_pull_at.to_date}
+      author: #{repository.author.name}
+      ---
+    CONTENT
+  end
+
+  # ---
+  # title: Course Name
+  # date: 2023-12-31
+  # author: The Author
+  # keywords: [one, two]
+  # ---
 end
