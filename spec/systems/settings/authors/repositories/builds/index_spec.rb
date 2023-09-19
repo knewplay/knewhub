2cb0583c5f0ed4f9464bc835d06b708633010eca
Jade <85654561+jp524@users.noreply.github.com>
2023-09-19 13:50:54 -0400

    Write tests for Build Settings::Author::Repositories::Builds#index

---
+++ spec/systems/settings/authors/repositories/builds/index_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+RSpec.describe 'Settings::Authors::Repositories::Builds#index', type: :system do
+  let(:log) { create(:log) }
+  let(:build) { log.build }
+  let(:repo) { build.repository }
+  let(:author) { repo.author }
+
+  context 'when logged in as an author' do
+    scenario 'displays build information' do
+      sign_in author.user
+      page.set_rack_session(author_id: author.id)
+
+      visit settings_author_repository_builds_path(build.id)
+      expect(page).to have_content("Repository '#{repo.title}'")
+      expect(page).to have_content("Action: #{build.action}")
+      expect(page).to have_content("Status: #{build.status}")
+    end
+
+    scenario 'displays logs content' do
+      sign_in author.user
+      page.set_rack_session(author_id: author.id)
+
+      visit settings_author_repository_builds_path(build.id)
+      expect(page).to have_content(log.content)
+    end
+
+    scenario 'updates the log tally' do
+      sign_in author.user
+      page.set_rack_session(author_id: author.id)
+
+      visit settings_author_repository_builds_path(build.id)
+      expect(page).to have_content("Status: In progress (1/#{build.max_log_count})")
+    end
+  end
+
+  context 'when not logged in as an author' do
+    scenario 'redirects to root path' do
+      visit settings_author_repository_builds_path(build.id)
+
+      expect(page).to have_current_path(root_path)
+      expect(page).to have_content('Please log in with GitHub.')
+    end
+  end
+end
