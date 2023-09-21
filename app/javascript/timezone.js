31739ea601afe2a104e6fa98df0604af03dfb398
Jade <85654561+jp524@users.noreply.github.com>
2023-09-21 15:10:33 -0400

    Create timezone.js

---
+++ app/javascript/timezone.js
@@ -0,0 +1,16 @@
+function setTimeZoneCookie() {
+  const tz = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+  setCookie('timezone', tz);
+
+  function setCookie(key, value) {
+    let expires = new Date();
+    let currentTime = expires.getTime();
+    let duration = (24 * 60 * 60 * 1000); // 24 hours
+    expires.setTime(currentTime + duration);
+
+    document.cookie = key + '=' + value + ';Expires=' + expires.toUTCString() + ';SameSite=Strict';
+  }
+}
+
+setTimeZoneCookie();
