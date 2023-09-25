c12a21ac3c374903241b8ae4a360b2257116188c
Jade <85654561+jp524@users.noreply.github.com>
2023-09-25 11:11:08 -0400

    Modify timezone.js to store one cookie for domain

--- app/javascript/timezone.js
+++ app/javascript/timezone.js
@@ -9,7 +9,7 @@ function setTimeZoneCookie() {
     let duration = (24 * 60 * 60 * 1000); // 24 hours
     expires.setTime(currentTime + duration);
 
-    document.cookie = key + '=' + value + ';Expires=' + expires.toUTCString() + ';SameSite=Strict';
+    document.cookie = key + '=' + value + ';Expires=' + expires.toUTCString() + ';SameSite=Strict' + ';Path=/';
   }
 }
 
