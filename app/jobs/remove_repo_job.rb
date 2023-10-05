a277699321a78745a36a1004fd29a31bf208b728
Jade <85654561+jp524@users.noreply.github.com>
2023-10-05 14:25:19 -0400

    Build RemoveRepoJob to include removal of webhook

---
+++ app/jobs/remove_repo_job.rb
@@ -0,0 +1,19 @@
+class RemoveRepoJob
+  include Sidekiq::Job
+
+  def perform(repository_id)
+    repository, directory = RepositoryDirectory.define(repository_id)
+    remove_webhook(repository)
+    delete_local_files(directory)
+    repository.destroy
+  end
+
+  def remove_webhook(repository)
+    client = Octokit::Client.new(access_token: repository.token)
+    client.remove_hook("#{repository.author.github_username}/#{repository.name}", repository.hook_id)
+  end
+
+  def delete_local_files(directory)
+    FileUtils.remove_dir(directory) if Dir.exist?(directory)
+  end
+end
---
+++ spec/jobs/remove_repo_job_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe RemoveRepoJob, type: :job do
+  let(:repo) { create(:repository, :real, hook_id: 436760769) }
+  let(:client) { Octokit::Client.new(access_token: repo.token) }
+
+  it 'queues the job' do
+    RemoveRepoJob.perform_async(repo.id)
+    expect(RemoveRepoJob).to have_enqueued_sidekiq_job(repo.id)
+  end
+
+  context 'when executing perform' do
+    before do
+      VCR.use_cassette('remove_repo') do
+        Sidekiq::Testing.inline! do
+          RemoveRepoJob.perform_async(repo.id)
+        end
+      end
+    end
+
+    it 'removes the webhook' do
+      VCR.use_cassette('get_repo_webhooks') do
+        get_repo_hooks = client.hooks("#{repo.author.github_username}/#{repo.name}")
+        expect(get_repo_hooks).to eq([])
+      end
+    end
+
+    it 'removes the record from the database' do
+      expect(Repository.exists?(repo.id)).to be false
+    end
+  end
+end
