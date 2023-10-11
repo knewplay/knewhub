ffc3346a702db9603c5b6d707b39904e50c05939
Jade <85654561+jp524@users.noreply.github.com>
2023-10-11 09:42:15 -0400

    WIP Update Rails to 7.1

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,11 @@ module Knewhub
     # Use ErrorsController to handle exceptions
     config.exceptions_app = routes
 
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w(assets tasks))
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
