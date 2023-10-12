8c39322f240b8f71f20b375c774668470b3886e4
Jade <85654561+jp524@users.noreply.github.com>
2023-10-12 11:13:09 -0400

    Implement new Rails 7.1 method #normalizes and add administrator model tests

--- app/models/administrator.rb
+++ app/models/administrator.rb
@@ -3,9 +3,15 @@ class Administrator < ApplicationRecord
 
   has_many :webauthn_credentials, dependent: :destroy
 
-  validates :name, presence: true, uniqueness: true
+  validates :name,
+            presence: true,
+            uniqueness: true,
+            format: { with: /\A[a-zA-Z0-9-]{0,39}\z/, message: 'can only contain alphanumeric characters and dashes' },
+            length: { maximum: 39 }
   validates :password, length: { minimum: 6 }, on: :create
   validates :permissions, presence: true
+
+  normalizes :name, with: ->(name) { name.strip.downcase }
 
   def multi_factor_enabled?
     webauthn_credentials.any?
--- spec/factories/administrators.rb
+++ spec/factories/administrators.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :administrator do
     name { 'admin' }
     password { 'password' }
+    permissions { 'admin' }
   end
 end
