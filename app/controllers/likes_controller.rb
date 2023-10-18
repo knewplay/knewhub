d4932e56cc396a2c6340ecef2240fe8ccc3c3c5f
Jade <85654561+jp524@users.noreply.github.com>
2023-10-18 15:15:54 -0400

    Build remove like button on answer view and integrate icons

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,12 +4,22 @@ class LikesController < ApplicationController
   def create
     @like = current_user.likes.build(like_params)
     @like.save
-    redirect_to answers_path(@like.answer.question)
+    redirect_to answers_path(question)
+  end
+
+  def destroy
+    @like = Like.find(params[:id])
+    @like.destroy
+    redirect_to answers_path(question)
   end
 
   private
 
   def like_params
     params.require(:like).permit(:answer_id)
+  end
+
+  def question
+    @like.answer.question
   end
 end
