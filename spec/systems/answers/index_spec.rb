651d9c9ae91a839088fd090e95365c2eb869cce7
Jade <85654561+jp524@users.noreply.github.com>
2023-10-18 15:22:16 -0400

    Refactor system tests for answers

--- spec/systems/answers/index_spec.rb
+++ spec/systems/answers/index_spec.rb
@@ -1,28 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe 'Answers#index', type: :system do
-  before(:all) do
-    @first_user = create(:user, email: 'email1@test.com')
-    second_user = create(:user, email: 'email2@test.com')
-    @question = create(:question)
-    @first_answer = create(:answer, user: @first_user, question: @question, body: 'This is the first answer.')
-    @second_answer = create(:answer, user: second_user, question: @question, body: 'This is the second answer.')
-  end
+  let!(:first_user) { create(:user, email: 'email1@test.com') }
+  let!(:second_user) { create(:user, email: 'email2@test.com') }
+  let!(:question) { create(:question) }
+  let!(:first_answer) { create(:answer, user: first_user, question:, body: 'This is the first answer.') }
+  let!(:second_answer) { create(:answer, user: second_user, question:, body: 'This is the second answer.') }
 
   context 'when logged in as a user' do
     scenario 'it displays all answers associated with a question' do
-      sign_in @first_user
-      visit answers_path(@question.id)
+      sign_in first_user
+      visit answers_path(question.id)
 
-      expect(page).to have_content(@question.body)
-      expect(page).to have_content(@first_answer.body)
-      expect(page).to have_content(@second_answer.body)
+      expect(page).to have_content(question.body)
+      expect(page).to have_content(first_answer.body)
+      expect(page).to have_content(second_answer.body)
     end
   end
 
   context 'when not logged in as a user' do
     scenario 'it redirects to login page' do
-      visit answers_path(@question.id)
+      visit answers_path(question.id)
 
       expect(page).to have_content('You need to log in or create an account before continuing.')
     end
