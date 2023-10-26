2708cbba1284c324a5ab5add1384baf66af6a897
Jade <85654561+jp524@users.noreply.github.com>
2023-10-26 11:51:57 -0400

    Revert "Replace Rails application credentials by environment variables"
    This reverts commit 797700661d87ce9af23a8cc8a3f2f25fff0c986a.

--- config/initializers/github.rb
+++ config/initializers/github.rb
@@ -1,5 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github,
-           ENV.fetch('GITHUB_APP_ID', Rails.application.credentials.github_id),
-           ENV.fetch('GITHUB_APP_SECRET', Rails.application.credentials.github_secret)
+  provider :github, Rails.application.credentials.github_id, Rails.application.credentials.github_secret
 end
