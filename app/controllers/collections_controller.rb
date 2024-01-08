7faefbbb8f4e771528699699de35478ee1387e91
Jade <85654561+jp524@users.noreply.github.com>
2024-01-08 10:17:14 -0500

    Prioritize rendering of Markdown files over HTML pages (#89)
    * Rendering of Markdown files performed by collections.html.erb instead of MarkdownHandler
    
    * Modify ExtractFrontMatterHelper to be SplitMarkdownHelper
    
    * Modify Collections system tests

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -6,7 +6,7 @@ class CollectionsController < ApplicationController
     file_path = "#{params[:owner]}/#{params[:name]}/index"
     render_not_found and return unless valid_render?(file_path, params[:owner], params[:name])
 
-    @front_matter = extract_front_matter_from_relative_path(file_path)
+    extract_markdown_file(file_path)
     render file_path
   end
 
@@ -33,8 +33,7 @@ class CollectionsController < ApplicationController
   def show_actions(file_path)
     respond_to do |format|
       format.html do
-        @front_matter = extract_front_matter_from_relative_path(file_path)
-        @markdown_content = Rails.root.join("repos/#{file_path}.md").read
+        extract_markdown_file(file_path)
         @questions = Question.where(repository: @repository, page_path: params[:path])
         render file_path
       end
@@ -69,8 +68,8 @@ class CollectionsController < ApplicationController
     send_file Rails.root.join("repos/#{file_path}.#{request.format.to_sym}").to_s
   end
 
-  def extract_front_matter_from_relative_path(file_path)
+  def extract_markdown_file(file_path)
     absolute_path = Rails.root.join("repos/#{file_path}.md").to_s
-    helpers.extract_front_matter(absolute_path)
+    @front_matter, @markdown_content = helpers.split_markdown(absolute_path)
   end
 end
--- config/initializers/markdown_handler.rb
+++ config/initializers/markdown_handler.rb
@@ -1,29 +1,8 @@
 # Custom handler to render Markdown files in HTML
 module MarkdownHandler
-  # erb template handler to allow support of embedded erb inside Markdown files
-  def self.erb
-    @erb ||= ActionView::Template.registered_template_handler(:erb)
-  end
-
-  def self.call(template, source)
-    erb_source = "#{erb.call(template, parsed_content(source))}.to_s"
-    extensions = {
-      fenced_code_blocks: true,
-      disable_indented_code_blocks: true
-    }
-
-    render_options = {
-      escape_html: true,
-      with_toc_data: true
-    }
-
-    "Redcarpet::Markdown.new(CustomRender.new(#{render_options}), #{extensions}).render(begin;#{erb_source};end)"
-  end
-
-  def self.parsed_content(source)
-    loader = FrontMatterParser::Loader::Yaml.new(allowlist_classes: [Date])
-    parsed = FrontMatterParser::Parser.new(:md, loader:).call(source)
-    parsed.content
+  # Rendering is performed by `layouts/collections`
+  def self.call(_template, _source)
+    ""
   end
 end
 
