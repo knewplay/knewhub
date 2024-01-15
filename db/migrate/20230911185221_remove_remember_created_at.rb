3151b3a7db84558aaf2c707898f077969fdd24a9
Jade <85654561+jp524@users.noreply.github.com>
2024-01-15 14:36:18 -0500

    Fix unsafe migrations (#100)
    * Add config for strong_migrations gem
    
    * Fix unsafe migrations

--- db/migrate/20230911185221_remove_remember_created_at.rb
+++ db/migrate/20230911185221_remove_remember_created_at.rb
@@ -1,5 +1,5 @@
 class RemoveRememberCreatedAt < ActiveRecord::Migration[7.0]
   def change
-    remove_column :users, :remember_created_at
+    safety_assured { remove_column :users, :remember_created_at }
   end
 end
--- db/migrate/20231004143201_remove_step_from_logs.rb
+++ db/migrate/20231004143201_remove_step_from_logs.rb
@@ -1,5 +1,5 @@
 class RemoveStepFromLogs < ActiveRecord::Migration[7.0]
   def change
-    remove_column :logs, :step
+    safety_assured { remove_column :logs, :step }
   end
 end
