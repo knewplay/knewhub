a30314683cbd1fecced23cc2d8c4161846a5dbd8
Jade <85654561+jp524@users.noreply.github.com>
2024-01-15 11:00:13 -0500

    Fix unsafe migrations

--- db/migrate/20230726192619_add_author_to_repositories.rb
+++ db/migrate/20230726192619_add_author_to_repositories.rb
@@ -1,6 +1,6 @@
 class AddAuthorToRepositories < ActiveRecord::Migration[7.0]
   def change
     add_reference :repositories, :author, foreign_key: true
-    remove_column :repositories, :owner
+    safety_assured { remove_column :repositories, :owner }
   end
 end
