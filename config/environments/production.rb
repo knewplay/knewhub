0d565863ea5bccbfbf6afcb7ab1e03dafc4e76f3
Jade <85654561+jp524@users.noreply.github.com>
2024-01-23 15:52:23 -0500

    Add ActionMailer settings for production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,6 +72,17 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "knewhub_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: URI(ENV.fetch('WEB_URL')).hostname }
+  config.action_mailer.smtp_settings = {
+    address:         'smtp-relay.brevo.com',
+    port:            587,
+    user_name:       ENV.fetch('BREVO_USERNAME'),
+    password:        ENV.fetch('BREVO_PASSWORD'),
+    authentication:  'plain',
+    enable_starttls: true,
+    open_timeout:    5,
+    read_timeout:    5 }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
