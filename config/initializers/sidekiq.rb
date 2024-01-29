e4bc694350567df66f3d900afcc70949f20141a2
Jade <85654561+jp524@users.noreply.github.com>
2024-01-29 12:08:45 -0500

    Add custom formatter for Sidekiq logs in production environment

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,10 @@
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV.fetch('REDIS_URL_SIDEKIQ', 'redis://localhost:6379/1') }
+  config.logger = Logger.new(STDOUT)
+  config.logger.formatter = proc do |severity, _time, _progname, msg|
+    "#{severity}: #{msg}\n"
+  end
+  config.logger.level = Logger::WARN if Rails.env == "production"
 end
 
 Sidekiq.configure_client do |config|
