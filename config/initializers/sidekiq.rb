c2e9e6fe1c9164f4e8eb08fa13f061a93f658de7
Jade <85654561+jp524@users.noreply.github.com>
2024-01-31 14:30:41 -0500

    Modify Sidekiq logger to provide more information

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,19 @@
+module Sidekiq
+  class Logger < ::Logger
+    module Formatters
+      class CustomFormatter < Base
+        def call(severity, time, program_name, message)
+          "#{severity}: #{message} | pid=#{::Process.pid} tid=#{tid}#{format_context} \n"
+        end
+      end
+    end
+  end
+end
+
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV.fetch('REDIS_URL_SIDEKIQ', 'redis://localhost:6379/1') }
   config.logger = Logger.new(STDOUT)
-  config.logger.formatter = proc do |severity, _time, _progname, msg|
-    "#{severity}: #{msg}\n"
-  end
+  config.logger.formatter = Sidekiq::Logger::Formatters::CustomFormatter.new
 end
 
 Sidekiq.configure_client do |config|
