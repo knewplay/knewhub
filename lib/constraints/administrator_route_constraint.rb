2760c06dc15375bb0c1f06188e4fdbb688b6dbe3
Jade <85654561+jp524@users.noreply.github.com>
2024-02-05 11:29:08 -0500

    Refactor AdministratorRouteConstraint

--- lib/constraints/administrator_route_constraint.rb
+++ lib/constraints/administrator_route_constraint.rb
@@ -1,26 +1,27 @@
 module Constraints
   class AdministratorRouteConstraint
     def matches?(request)
-      administrator_signed_in?(request)
+      @request = request
+      administrator_signed_in?
     end
 
     private
 
-    def current_administrator(request)
-      validate_session(request)
-      if request.session[:administrator_id]
-        @current_administrator ||= Administrator.find(request.session[:administrator_id])
-      end
+    def current_administrator
+      validate_session
+      return unless @request.session[:administrator_id]
+
+      @current_administrator ||= Administrator.find(@request.session[:administrator_id])
     end
 
-    def administrator_signed_in?(request)
-      !!current_administrator(request)
+    def administrator_signed_in?
+      !!current_administrator
     end
 
-    def validate_session(request)
-      return if request.session[:administrator_expires_at].nil?
+    def validate_session
+      return if @request.session[:administrator_expires_at].nil?
 
-      request.session[:administrator_id] = nil if request.session[:administrator_expires_at] < Time.current
+      @request.session[:administrator_id] = nil if @request.session[:administrator_expires_at] < Time.current
     end
   end
 end
