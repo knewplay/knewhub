3ddcc9e4486a60d0676c40966297be7ec6148ceb
Jade <85654561+jp524@users.noreply.github.com>
2024-02-12 12:47:14 -0500

    Update RemoveRepoJob

--- app/jobs/remove_repo_job.rb
+++ app/jobs/remove_repo_job.rb
@@ -1,14 +1,16 @@
 class RemoveRepoJob
   include Sidekiq::Job
 
-  def perform(github_username, name, hook_id, token, directory)
-    remove_webhook(github_username, name, hook_id, token)
+  def perform(author_id, full_name, hook_id)
+    github_client = Author.find(author_id).github_client
+    directory = Rails.root.join('repos', full_name)
+
+    remove_webhook(github_client, full_name, hook_id)
     delete_local_files(directory)
   end
 
-  def remove_webhook(github_username, name, hook_id, token)
-    client = Octokit::Client.new(access_token: token)
-    client.remove_hook("#{github_username}/#{name}", hook_id)
+  def remove_webhook(github_client, full_name, hook_id)
+    github_client.remove_hook(full_name, hook_id)
   end
 
   def delete_local_files(directory)
--- spec/systems/settings/authors/repositories/destroy_spec.rb
+++ spec/systems/settings/authors/repositories/destroy_spec.rb
@@ -2,13 +2,12 @@ require 'rails_helper'
 
 RSpec.describe 'Settings::Authors::Repositories#destroy', type: :system do
   before(:all) do
-    @repo = create(:repository, :real, hook_id: 436_760_769)
+    @repo = create(:repository, :real, hook_id: 440_848_647)
     @directory = Rails.root.join('repos', @repo.author.github_username, @repo.name)
     FileUtils.mkdir_p(@directory)
 
     @before_count = Repository.count
     sign_in @repo.author.user
-    page.set_rack_session(author_id: @repo.author.id)
 
     visit edit_settings_author_repository_path(@repo.id)
 
