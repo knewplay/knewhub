563ef74c4adfeb3cd440f83db9e5a566fc456163
Jade <85654561+jp524@users.noreply.github.com>
2024-02-12 15:57:45 -0500

    Update Rails credentials

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -2,7 +2,6 @@ VCR.configure do |config|
   config.cassette_library_dir = 'spec/cassettes'
   config.hook_into :webmock
   config.ignore_localhost = true
-  config.filter_sensitive_data('<TOKEN>') { Rails.application.credentials.pat }
   config.filter_sensitive_data('<SECRET>') { Rails.application.credentials.webhook_secret }
   config.filter_sensitive_data('<HOST_URL>') { Rails.application.credentials.host_url }
   config.filter_sensitive_data('<GITHUB_CLIENT_ID>') { Rails.application.credentials.dig(:github, :client_id) }
