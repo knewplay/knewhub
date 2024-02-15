d78e7c4e92aa326993f0d07a3e4ced58e9b71359
Jade <85654561+jp524@users.noreply.github.com>
2024-02-15 13:02:49 -0500

    Modify system test for creating Repository

--- spec/systems/settings/authors/repositories/create_spec.rb
+++ spec/systems/settings/authors/repositories/create_spec.rb
@@ -31,8 +31,11 @@ RSpec.describe 'Settings::Authors::Repositories#create', type: :system do
           click_on 'Create Repository'
 
           expect(page).to have_content('Repository creation process was initiated.')
-          expect(Repository.last.title).to eq('Test Repo')
-          expect(Repository.last.branch).to eq('main')
+          repo = Repository.last
+          expect(repo.title).to eq('Test Repo')
+          expect(repo.branch).to eq('main')
+          expect(repo.full_name).to eq('user/repo_name')
+          expect(repo.uid).to eq(123_456_789)
         end
       end
 
@@ -44,7 +47,11 @@ RSpec.describe 'Settings::Authors::Repositories#create', type: :system do
           click_on 'Create Repository'
 
           expect(page).to have_content('Repository creation process was initiated.')
-          expect(Repository.last.branch).to eq('some_branch')
+          repo = Repository.last
+          expect(repo.title).to eq('Test Repo')
+          expect(repo.branch).to eq('some_branch')
+          expect(repo.full_name).to eq('user/repo_name')
+          expect(repo.uid).to eq(123_456_789)
         end
       end
 
@@ -58,36 +65,37 @@ RSpec.describe 'Settings::Authors::Repositories#create', type: :system do
       end
     end
 
-    context 'when using the Build process', skip: 'To be finalized' do
+    context 'when using the Build process' do
       before(:all) do
+        # HTTP request required to clone repository using Octokit client
+        VCR.turn_off!
+        WebMock.allow_net_connect!
         author = create(:author, :real)
+        github_installation = create(:github_installation, :real, author:)
         sign_in author.user
 
-        VCR.use_cassettes([{ name: 'get_installation_access_token' }, { name: 'get_repos' }]) do
-          visit new_settings_author_repository_path(full_name: 'jp524/test-repo', uid: 123_456_789)
-        end
+        visit new_settings_author_repository_path(full_name: 'jp524/test-repo', uid: 663_068_537)
 
         fill_in('Title', with: 'Test Repo')
 
         Sidekiq::Testing.inline! do
-          VCR.use_cassettes([{ name: 'get_installation_access_token', options: { allow_playback_repeats: true } },
-                             { name: 'create_github_webhook' },
-                             { name: 'test_github_webhook' }]) do
-            click_on 'Create Repository'
-          end
+          click_on 'Create Repository'
         end
 
-        @repo = Repository.last
+        @repo = github_installation.repositories.last
         @build = @repo.builds.first
       end
 
       after(:all) do
         @repo.reload
         FileUtils.remove_dir(@repo.storage_path)
+        VCR.turn_on!
+        WebMock.disable_net_connect!
+      end
 
-        VCR.use_cassettes([{ name: 'get_installation_access_token' }, { name: 'delete_github_webhook' }]) do
-          @repo.github_installation.github_client.remove_hook(@repo.full_name, 460_475_619)
-        end
+      it 'creates a Repository' do
+        expect(@repo.full_name).to eq('jp524/test-repo')
+        expect(@repo.uid).to eq(663_068_537)
       end
 
       it "creates an associated Build with action 'create'" do
@@ -95,27 +103,19 @@ RSpec.describe 'Settings::Authors::Repositories#create', type: :system do
       end
 
       it 'creates the first log' do
-        expect(@build.logs.first.content).to eq('GitHub webhook successfully created. Now testing...')
+        expect(@build.logs.first.content).to eq('Repository successfully cloned.')
       end
 
       it 'creates the second log' do
-        expect(@build.logs.second.content).to eq('GitHub webhook successfully tested.')
+        expect(@build.logs.second.content).to eq('Repository description successfully updated from GitHub.')
       end
 
-      it 'creates the third log' do
-        expect(@build.logs.third.content).to eq('Repository successfully cloned.')
+      it 'with third log' do
+        expect(@build.logs.third.content).to eq('Questions successfully parsed.')
       end
 
       it 'creates the fourth log' do
-        expect(@build.logs.fourth.content).to eq('Repository description successfully updated from GitHub.')
-      end
-
-      it 'with fifth log' do
-        expect(@build.logs.fifth.content).to eq('Questions successfully parsed.')
-      end
-
-      it 'creates the sixth log' do
-        expect(@build.logs[5].content).to eq('index.md file successfully generated.')
+        expect(@build.logs.fourth.content).to eq('index.md file successfully generated.')
       end
 
       it "sets Build status to 'Complete'" do
