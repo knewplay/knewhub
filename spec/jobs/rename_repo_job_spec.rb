3c04a1a0be5052b403261b21250d84f4afca1333
Jade <85654561+jp524@users.noreply.github.com>
2024-02-22 09:31:15 -0500

    Change name of job RespondWebhookRenameRepoJob

--- spec/jobs/rename_repo_job_spec.rb
+++ spec/jobs/rename_repo_job_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe RespondWebhookRenameRepoJob do
+RSpec.describe RenameRepoJob do
   before(:all) do
     # Create repository and its directory in `repos` folder
     @repo = create(:repository)
