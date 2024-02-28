c7df4affa7d34a4a1fcc9ac2b8a175c6b4e3e697
Jade <85654561+jp524@users.noreply.github.com>
2024-02-28 10:06:56 -0500

    Clarify existing code for CustomRender

--- config/initializers/custom_render.rb
+++ config/initializers/custom_render.rb
@@ -1,4 +1,4 @@
-# Custom renderer that sets a custom class for code blocks
+# Custom renderer for Markdown files
 require 'rouge/plugins/redcarpet'
 
 class CustomRender < Redcarpet::Render::HTML
@@ -26,15 +26,18 @@ class CustomRender < Redcarpet::Render::HTML
   private
 
   def process_custom_tags(text)
-    if (t = text.match(/(\[codefile )(.+)(\])/)) # [codefile <relative_path>]
+    # [codefile <relative_path>]
+    if (t = text.match(/(\[codefile )(.+)(\])/))
       process_codefile(t[2])
-    elsif (t = text.match(%r{(\[details )(.+)(\])(.+)(\[/details\])})) # [details Hint]content[/details]
+    # [details Hint]content[/details]
+    elsif (t = text.match(%r{(\[details )(.+)(\])(.+)(\[/details\])}))
       process_details(t[2], t[4])
     else
       "<p>#{text}</p>"
     end
   end
 
+  # Allow code blocks to be created from a separate file in the same repository
   def process_codefile(relative_path)
     absolute_path = Rails.root.to_s + RequestPath.define_base_url + relative_path
     data = File.read(absolute_path)
