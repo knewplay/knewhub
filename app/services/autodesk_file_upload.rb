335d76c63b3d9e934e6c748dcb912dc46396e508
Jade <85654561+jp524@users.noreply.github.com>
2024-03-07 10:09:04 -0500

    Refactor AutodeskFileUpload service

--- app/services/autodesk_file_upload.rb
+++ app/services/autodesk_file_upload.rb
@@ -9,16 +9,17 @@ class AutodeskFileUpload < Autodesk
   def upload_file_for_viewer
     return unless allowed_to_upload_file?
 
-    base64_urn = start_upload
+    # Upload source file to Autodesk storage bucket
+    upload_key, upload_url = bucket_signed_url.values
+    start_upload(upload_url)
+    base64_object_urn = finalize_upload(upload_key)
 
-    translate_job_response = translate_to_svf(base64_urn)
-    urn_encoded = JSON.parse(translate_job_response.body)['urn']
+    # Translate source file into SVF format for Viewer
+    base64_file_urn = translate_to_svf(base64_object_urn)
+    translation_job_response_body = check_translation_job_complete(base64_file_urn)
+    update_build_logs(translation_job_response_body)
 
-    verify_response = verify_job_complete(urn_encoded)
-    verify_response_as_json = JSON.parse(verify_response.body)
-
-    update_build(verify_response_as_json)
-    verify_response_as_json['urn'] if verify_response_as_json['status'] == 'success'
+    base64_file_urn if translation_job_response_body['status'] == 'success'
   end
 
   private
@@ -51,70 +52,64 @@ class AutodeskFileUpload < Autodesk
       { 'Content-Type': 'application/json', Authorization: "Bearer #{@access_token}" }
     )
     response_as_json = JSON.parse(response.body)
-    { upload_key: response_as_json['uploadKey'], urls: response_as_json['urls'] }
+    { upload_key: response_as_json['uploadKey'], upload_url: response_as_json['urls'].first }
   end
 
-  def finalize_upload(upload_key)
-    request_params = { ossbucketKey: upload_key, ossSourceFileObjectKey: CGI.escape(@filepath), access: 'full',
-                        uploadKey: upload_key }
-    response = @conn.post(
-      "/oss/v2/buckets/#{@bucket_key}/objects/#{CGI.escape(@filepath)}/signeds3upload",
-      request_params.to_json,
-      { 'Content-Type': 'application/json', Authorization: "Bearer #{@access_token}" }
-    )
-    response_as_json = JSON.parse(response.body)
-    urn = response_as_json['objectId']
-    Base64.urlsafe_encode64(urn)
-  end
-
-  def translate_to_svf(base64_urn)
-    request_params = {
-      input: { urn: base64_urn },
-      output: { formats: [{ type: 'svf', views: %w[2d 3d] }] }
-    }
-    @conn.post(
-      '/modelderivative/v2/designdata/job',
-      request_params.to_json,
-      { 'Content-Type': 'application/json', Authorization: "Bearer #{@access_token}", 'x-ads-force': 'true' }
-    )
-  end
-
-  def verify_job_complete(base_64_urn)
-    is_complete = false
-
-    until is_complete
-      response = @conn.get(
-        "/modelderivative/v2/designdata/#{base_64_urn}/manifest", nil, { Authorization: "Bearer #{@access_token}" }
-      )
-      response_as_json = JSON.parse(response.body)
-      response_as_json['progress'] == 'complete' ? is_complete = true : sleep(1)
-    end
-
-    response
-  end
-
-  def start_upload
-    upload_key, urls = bucket_signed_url.values
-    upload_url = urls.first
-
+  def start_upload(upload_url)
     conn = Faraday.new(url: upload_url)
     conn.put(
       '',
       File.binread(@filepath),
       { 'Content-Type': 'application/octet-stream' }
     )
-
-    finalize_upload(upload_key)
   end
 
-  def update_build(response_as_json)
-    if response_as_json['status'] == 'success'
+  def finalize_upload(upload_key)
+    request_params = { ossbucketKey: upload_key, ossSourceFileObjectKey: CGI.escape(@filepath),
+                       access: 'full', uploadKey: upload_key }
+    response = @conn.post(
+      "/oss/v2/buckets/#{@bucket_key}/objects/#{CGI.escape(@filepath)}/signeds3upload",
+      request_params.to_json,
+      { 'Content-Type': 'application/json', Authorization: "Bearer #{@access_token}" }
+    )
+    response_as_json = JSON.parse(response.body)
+    object_urn = response_as_json['objectId']
+    Base64.urlsafe_encode64(object_urn)
+  end
+
+  def translate_to_svf(base64_object_urn)
+    request_params = { input: { urn: base64_object_urn }, output: { formats: [{ type: 'svf', views: %w[2d 3d] }] } }
+    response = @conn.post(
+      '/modelderivative/v2/designdata/job',
+      request_params.to_json,
+      { 'Content-Type': 'application/json', Authorization: "Bearer #{@access_token}", 'x-ads-force': 'true' }
+    )
+    response_body = JSON.parse(response.body)
+    response_body['urn']
+  end
+
+  def check_translation_job_complete(base64_file_urn)
+    is_complete = false
+
+    until is_complete
+      response = @conn.get(
+        "/modelderivative/v2/designdata/#{base64_file_urn}/manifest", nil, { Authorization: "Bearer #{@access_token}" }
+      )
+      response_body = JSON.parse(response.body)
+      response_body['progress'] == 'complete' ? is_complete = true : sleep(1)
+    end
+
+    response_body
+  end
+
+  def update_build_logs(translation_job_response_body)
+    if translation_job_response_body['status'] == 'success'
       @build.logs.create(content: "'#{@filepath}' successfully uploaded to Autodesk servers.")
     else
       content = <<~MSG
         Failed to upload '#{@filepath}' to Autodesk servers.
-        Status: #{verify_response_as_json['derivatives']['status']}.
-        Message: #{verify_response_as_json['derivatives']['messages']}
+        Status: #{translation_job_response_body['derivatives']['status']}.
+        Message: #{translation_job_response_body['derivatives']['messages']}
       MSG
       @build.logs.create(content:, failure: true)
     end
